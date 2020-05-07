285d2a13a06b49654ce47aff27f6f3de02258d1e
Nowosad <tupiszakaczki@gmail.com>
2020-05-07 14:36:12 +0200

    adds prepare_course_materials

---
+++ R/prepare_course_materials.R
@@ -0,0 +1,27 @@
+library(purrr)
+library(knitr)
+dir.create("slides")
+slides_rmd = "index.Rmd"
+
+# extracts code -----------------------------------------------------------
+purl(slides_rmd, 
+     output = "slides/slides_code.R",
+     documentation = 0)
+
+# creates pdf -------------------------------------------------------------
+pagedown::chrome_print(slides_rmd,
+                       output = "slides/slides.pdf",
+                       wait = 10)
+
+# creates a workshop zip --------------------------------------------------
+exercises = dir("exercises", full.names = TRUE)
+data = dir("data", full.names = TRUE)
+
+file.remove("ialena-2020.zip")
+zip("ialena-2020.zip",
+    files = c("ialena-2020.Rproj",
+              "slides/slides_code.R",
+              "slides/slides.pdf",
+              exercises,
+              data),
+    extras = "")
